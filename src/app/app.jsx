4639f36b89563bd669532196bb2ec0f5b1419ab9
Jordaan <jordaanm@gmail.com>
2020-06-25 08:23:32 +1000

    fix: top navbar works

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,23 +1,28 @@
 import * as React from 'react'
-import './app.scss'
-import "@blueprintjs/core/lib/css/blueprint.css";
 import { Tab, Tabs } from '@blueprintjs/core';
+import { useObserver } from 'mobx-react-lite';
 import { useStores } from '../stores/util';
 import { CardDesigns } from '../design/design';
 import { DataSets } from '../data-set/data-set';
 
+import "@blueprintjs/core/lib/css/blueprint.css";
+import './app.scss'
+
 const AppContainer = (props) => {
   const {ui} = useStores();
-  const tabChange = id => ui.currentSection = id;
 
-  return (
-    <div className="app-container">
-      <Tabs id='AppToolbarTabs' onChange={tabChange} selectedTabId={ui.currentSection} className="full-xy">
-        <Tab id='design' title='Card Designs' panel={<CardDesigns />} />
-        <Tab id='data' title='Data Sets' panel={<DataSets />} />
-      </Tabs>
-    </div>
-  );
+  return useObserver(() => {
+    const tabChange = id => ui.currentSection = id;
+
+    return (
+      <div className="app-container">
+        <Tabs id='AppToolbarTabs' onChange={tabChange} selectedTabId={ui.currentSection} className="full-xy">
+          <Tab id='design' title='Card Designs' panel={<CardDesigns />} />
+          <Tab id='data' title='Data Sets' panel={<DataSets />} />
+        </Tabs>
+      </div>
+    );
+  });
 };
 
 export default AppContainer;
